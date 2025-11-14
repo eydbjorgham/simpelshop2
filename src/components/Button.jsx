ef64641f4ce95ade7e39aaf33e2ab86b7f0e6e8e
eydbjorgham <eyha0001@stud.kea.dk>
2025-11-14 10:00:39 +0100

    Add Button component and integrate it into the Home page

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,14 @@
-const Button = () => {
-    return (  );
-}
+// import { useState } from "react";
+
+const Button = ({ text, onClick }) => {
+  return (
+    <button
+      className={"border-0 border-b-2 border-(--h1-color) bg-transparent p-2 w-fit font-semibold hover:cursor-pointer hover:scale-105 transition-transform duration-200 z-1 text-(--h1-color) text-3xl"}
+    //   onClick={onClick}
+    >
+      {text}
+    </button>
+  );
+};
  
 export default Button;
