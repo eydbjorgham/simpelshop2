dc18e1854b7c280f74ca5276c9792091848ec0e7
Aljo0004 <aljo0004@stud.kea.dk>
2025-11-17 15:42:51 +0100

    pl-2

--- src/components/CategoryDropdown.jsx
+++ src/components/CategoryDropdown.jsx
@@ -20,8 +20,8 @@ const CategoryDropdown = ({ categories }) => {
     <select
       value={selectedCategory}
       onChange={handleChange}
-      className="m-6 h-10 border text-l pr-1 border-gray-300 rounded-2xl bg-white shadow-xs hover:border-blue-500 focus:border-blue-500 outline-none cursor-pointer capitalize">
-      <option value="" className='text-center'>Alle kategorier</option>
+      className="m-6 h-10 border text-l pr-1 border-gray-300 rounded-2xl bg-white shadow-xs hover:border-blue-500 focus:border-blue-500 outline-none cursor-pointer pl-2 capitalize">
+      <option value="">Alle kategorier</option>
     
       {categories.map((category, index) => (
         <option key={index} value={category}>
