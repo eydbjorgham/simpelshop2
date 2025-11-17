5d3d4df5cdcfb152f52e4da3acbef958f2b48795
Aljo0004 <aljo0004@stud.kea.dk>
2025-11-17 14:54:35 +0100

    Add Categories component and CategoryDropdown for product filtering

--- src/app/(routes)/products/page.js
+++ src/app/(routes)/products/page.js
@@ -2,11 +2,18 @@ import Navbar from "@/components/Navbar";
 import { Suspense } from "react";
 import ProductCard from "@/components/ProductCard";
 import Kurv from "@/components/Kurv";
+import Categories from "@/components/Categories";
 
 export default function Home({ searchParams }) {
   return (  
     <section>
       <Navbar />
+      <div className="flex flex-row items-center justify-between">
+       <Categories />
+       <input   type="text" 
+  placeholder="Søg efter produkter..."
+  className="p-3 border h-10 border-gray-300 bg-white rounded-lg mr-6"></input>
+       </div>
       <div className="flex gap-6 p-6">
         <div className="flex-1">
           <Suspense>
---
+++ src/components/CategoryDropdown.jsx
@@ -0,0 +1,35 @@
+"use client";
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+
+const CategoryDropdown = ({ categories }) => {
+  const router = useRouter();
+  const [selectedCategory, setSelectedCategory] = useState('');
+
+  const handleChange = (e) => {
+    const category = e.target.value;
+    setSelectedCategory(category);
+    if (category) {
+      router.push(`?category=${category}`);
+    } else {
+      router.push('/products');
+    }
+  };
+
+  return (
+    <select
+      value={selectedCategory}
+      onChange={handleChange}
+      className="m-6 h-10 border text-l border-gray-300 rounded-2xl bg-white shadow-xs hover:border-blue-500 focus:border-blue-500 outline-none cursor-pointer">
+      <option value="" className='text-center'>Alle kategorier</option>
+    
+      {categories.map((category, index) => (
+        <option key={index} value={category}>
+          {category}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+export default CategoryDropdown;
