27b1717dcb4a7cbf79d6ada7cf027d5028899529
eydbjorgham <eyha0001@stud.kea.dk>
2025-11-17 14:01:19 +0100

    Refactor components for improved styling and structure; remove unused code and enhance button functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,19 @@
 import Link from "next/link";
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["300", "400", "600"],
+});
 
 const Navbar = () => {
   return (
-    <nav>
+    <nav className={inter.className}>
       <ul className="sticky flex justify-center space-x-24 p-8 text-xl">
-        <li>
+        <li className="w-fit border-0 border-b-2 border-(--h1-color) transition-transform duration-200 hover:scale-105 hover:cursor-pointer">
           <Link href="/">Home</Link>
         </li>
-        <li>
+        <li className="w-fit border-0 border-b-2 border-(--h1-color) transition-transform duration-200 hover:scale-105 hover:cursor-pointer">
           <Link href="/products">Products</Link>
         </li>
       </ul>
